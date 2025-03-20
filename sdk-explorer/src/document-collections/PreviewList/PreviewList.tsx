5a88e25d6fe28888b5bf53bc685b60625516e9b0
Cole Peters <cole@colepeters.com>
2025-03-20 15:48:22 -0500

    feat!: removes auth & user profile example; updates for new hooks (#15)

--- sdk-explorer/src/document-collections/PreviewList/PreviewList.tsx
+++ sdk-explorer/src/document-collections/PreviewList/PreviewList.tsx
@@ -1,5 +1,5 @@
 import { DocumentHandle } from '@sanity/sdk'
-import { useDocuments, usePreview } from '@sanity/sdk-react/hooks'
+import { useInfiniteList, useProjection } from '@sanity/sdk-react'
 import { Button, Card, Flex, Inline, Spinner, Stack, Text } from '@sanity/ui'
 import { Suspense, useRef } from 'react'
 import ExampleLayout from '../../ExampleLayout'
@@ -14,18 +14,36 @@ function Loading() {
   )
 }
 
+// @todo replace with type from SDK
+interface ProjectionResults {
+  results: {
+    title: string
+    subtitle: string
+    coverImage: string
+  }
+  isPending: boolean
+}
+
 // The DocumentPreview component uses the `usePreview` hook to render a document preview interface
 function DocumentPreview({ document }: { document: DocumentHandle }) {
   // Generate a ref for the outer element
-  // This keeps the usePreview hook from resolving if the preview is not currently displayed in the viewport
+  // This keeps the useProjection hook from firing if the preview is not currently displayed in the viewport
   const ref = useRef(null)
 
-  // Get the preview's title, subtitle, and media fields,
-  // plus an `isPending` flag to indicate if preview value resolutions are pending
+  // Project the title, subtitle, and cover image values for the document,
+  // plus an `isPending` flag to indicate if projection value resolutions are pending
   const {
-    results: { title, subtitle, media },
+    results: { title, subtitle, coverImage },
     isPending,
-  } = usePreview({ document, ref })
+  }: ProjectionResults = useProjection({
+    document,
+    ref,
+    projection: `{
+      title,
+      'subtitle': array::join(authors[]->{'name': firstName + ' ' + lastName}.name, ', '),
+      'coverImage': cover.asset->url,
+    }`,
+  })
 
   return (
     <Button
@@ -37,11 +55,9 @@ function DocumentPreview({ document }: { document: DocumentHandle }) {
       style={{ opacity: isPending ? 0.5 : 1 }}
     >
       <Inline space={4}>
-        {media?.type === 'image-asset' && (
-          <Card tone='transparent' shadow={2}>
-            <img src={media.url} alt='' width='128' />
-          </Card>
-        )}
+        <Card tone='transparent' shadow={2}>
+          <img src={coverImage} alt='' width='128' />
+        </Card>
         <Stack space={3}>
           <Text as='h2' weight='medium' size={4}>
             {title}
@@ -56,9 +72,9 @@ function DocumentPreview({ document }: { document: DocumentHandle }) {
 function PreviewList() {
   // Use the `useDocuments` hook to return an index of document handles for all of our 'book' type documents
   // Sort the documents by the author's last name, then the release date
-  const { results: books, isPending } = useDocuments({
+  const { data: books, isPending } = useInfiniteList({
     filter: '_type == "book"',
-    sort: [
+    orderings: [
       { field: 'authors[0]->lastName', direction: 'asc' },
       { field: 'releaseDate', direction: 'asc' },
     ],
@@ -76,7 +92,7 @@ function PreviewList() {
     <ExampleLayout
       title='Preview list'
       codeUrl='https://github.com/sanity-io/sdk-examples/blob/main/sdk-explorer/src/document-collections/PreviewList/PreviewList.tsx'
-      hooks={['useDocuments', 'usePreview']}
+      hooks={['useInfiniteList', 'useProjection']}
       styling='Sanity UI'
     >
       <Stack space={4}>
