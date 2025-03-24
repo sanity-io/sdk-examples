8052cff336a808fab026b6c13603e55e588b8dee
Cole Peters <cole@colepeters.com>
2025-03-24 12:33:29 -0500

    chore!: remove previous example studio

--- studio-sdk-examples/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import {authorType} from './authorType'
-import {bookType} from './bookType'
-import {publisherType} from './publisherType'
-
-export const schemaTypes = [authorType, bookType, publisherType]
