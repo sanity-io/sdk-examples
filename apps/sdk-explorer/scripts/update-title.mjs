93aa60b1b156b09b66f7a9546583fcca80b56318
Cole Peters <cole@colepeters.com>
2025-05-15 13:36:26 -0500

    chore: please work

--- apps/sdk-explorer/scripts/update-title.mjs
+++ apps/sdk-explorer/scripts/update-title.mjs
@@ -4,19 +4,20 @@ import fs from 'fs'
 const indexHtmlPath = './.sanity/runtime/index.html'
 
 function updateTitle() {
+  console.log('Updating site title…')
+
   try {
     if (!fs.existsSync(indexHtmlPath)) {
       console.error(`File not found: ${indexHtmlPath}`)
       return
     }
 
-    let html = fs.readFileSync(indexHtmlPath, 'utf8')
-
     const titleRegex = /<title>(.*?)<\/title>/i
 
-    html = html.replace(titleRegex, '<title>Sanity App SDK Explorer</title>')
+    const baseHtml = fs.readFileSync(indexHtmlPath, 'utf8')
+    const newHtml = baseHtml.replace(titleRegex, '<title>Sanity App SDK Explorer</title>')
 
-    fs.writeFileSync(indexHtmlPath, html)
+    fs.writeFileSync(indexHtmlPath, newHtml)
 
     console.log('Updated site title')
   } catch (e) {
