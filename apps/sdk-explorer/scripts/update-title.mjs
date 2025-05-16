476d7a51a82ac1d690c8282a0bd74f428d690567
Cole Peters <cole@colepeters.com>
2025-05-16 13:54:54 -0500

    feat!: update vite config & monogram (#57)
    * feat!: update vite config & monogram
    
    Updates the Explorer to use the current methodology for building and
    serving (via Sanity CLI) and updates the Vite config methodology
    accordingly (extending via defineCliConfig). Also updates the Sanity
    monogram to match the new brand ID.
    
    * chore: add script to update site title

---
+++ apps/sdk-explorer/scripts/update-title.mjs
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+import fs from 'fs'
+
+const indexHtmlPath = 'dist/index.html'
+
+function updateTitle() {
+  console.log('Updating site title…')
+
+  try {
+    if (!fs.existsSync(indexHtmlPath)) {
+      console.error(`File not found: ${indexHtmlPath}`)
+      return
+    }
+
+    const titleRegex = /<title>(.*?)<\/title>/i
+
+    const baseHtml = fs.readFileSync(indexHtmlPath, 'utf8')
+    const newHtml = baseHtml.replace(titleRegex, '<title>Sanity App SDK Explorer</title>')
+
+    fs.writeFileSync(indexHtmlPath, newHtml)
+
+    console.log('Updated site title')
+  } catch (e) {
+    console.error('Error updating site title:', e)
+  }
+}
+
+updateTitle()
--- apps/sdk-explorer/src/App.tsx
+++ apps/sdk-explorer/src/App.tsx
@@ -1,76 +1,53 @@
-import {hues} from '@sanity/color'
-import {SanityMonogram} from '@sanity/logos'
-import {Box, Card, Container, Flex, Inline, Text} from '@sanity/ui'
-import {type JSX} from 'react'
-import {BrowserRouter, Link, Route, Routes} from 'react-router'
+import './inter.css'
+
+import {ResourceProvider} from '@sanity/sdk-react'
+import {Flex, Spinner, ThemeProvider} from '@sanity/ui'
+import {buildTheme, ThemeConfig} from '@sanity/ui/theme'
+import {type JSX, StrictMode, Suspense} from 'react'
 import {createGlobalStyle} from 'styled-components'
 
-import ScrollOnPathChange from './components/ScollOnPathChange'
-import DocumentSearch from './examples/document-collections/DocumentSearch/DocumentSearch'
-import DocumentTable from './examples/document-collections/DocumentTable/DocumentTable'
-import PreviewGrid from './examples/document-collections/PreviewGrid/PreviewGrid'
-import PreviewList from './examples/document-collections/PreviewList/PreviewList'
-import MoviesByActor from './examples/groq/MoviesByActor/MoviesByActor'
-import Home from './Home'
+import Explorer from './Explorer'
 
-const Body = createGlobalStyle`
+const themeConfig: ThemeConfig = {
+  media: [320, 600],
+}
+
+const theme = buildTheme(themeConfig)
+
+const sanityConfig = {
+  projectId: 'v28v5k8m',
+  dataset: 'production',
+}
+
+const GlobalStyle = createGlobalStyle`
   body {
-    background-color: ${hues.gray['50'].hex};
+    margin: 0;
   }
 `
 
+function Loading() {
+  return (
+    <Flex align="center" justify="center" style={{inlineSize: '100dvw', blockSize: '100dvh'}}>
+      <Spinner />
+    </Flex>
+  )
+}
+
 export default function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Body />
-      <ScrollOnPathChange />
-      <Card style={{position: 'relative'}} tone="transparent">
-        <Card
-          tone="transparent"
-          shadow={1}
-          marginBottom={5}
-          style={{
-            position: 'sticky',
-            top: 0,
-            zIndex: 3,
-            backgroundColor: 'white',
-          }}
-        >
-          <Box paddingX={4}>
-            <Flex align="center" justify="space-between" style={{height: 52}}>
-              <Text as="h1" size={2} weight="medium">
-                <Link to="/" style={{color: 'inherit'}}>
-                  <Flex align="center" gap={3}>
-                    <SanityMonogram style={{margin: 0}} />
-                    SDK Explorer
-                  </Flex>
-                </Link>
-              </Text>
-              <Inline space={4}>
-                <Text weight="medium" size={1}>
-                  <a href="/">Home</a>
-                </Text>
-                <Text weight="medium" size={1}>
-                  <a href="https://sanity.io/docs/app-sdk">App SDK Docs</a>
-                </Text>
-                <Text weight="medium" size={1}>
-                  <a href="https://github.com/sanity-io/sdk">GitHub</a>
-                </Text>
-              </Inline>
-            </Flex>
-          </Box>
-        </Card>
-        <Container width={2} padding={4}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/document-collections/document-search" element={<DocumentSearch />} />
-            <Route path="/document-collections/document-table" element={<DocumentTable />} />
-            <Route path="/document-collections/preview-list" element={<PreviewList />} />
-            <Route path="/document-collections/preview-grid" element={<PreviewGrid />} />
-            <Route path="/groq/movies-by-actor" element={<MoviesByActor />} />
-          </Routes>
-        </Container>
-      </Card>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <StrictMode>
+        <GlobalStyle />
+        <Suspense fallback={<Loading />}>
+          <ResourceProvider
+            projectId={sanityConfig.projectId}
+            dataset={sanityConfig.dataset}
+            fallback={<Loading />}
+          >
+            <Explorer />
+          </ResourceProvider>
+        </Suspense>
+      </StrictMode>
+    </ThemeProvider>
   )
 }
